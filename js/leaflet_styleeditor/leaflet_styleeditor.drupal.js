ac067564d9d5e4a295caa3eaa7e5c769fda2a287
olivierh65 <ol65@sfr.fr>
2024-11-20 10:37:03 +0100

    mise à plat - non fonctionnel

--- js/leaflet_styleeditor/leaflet_styleeditor.drupal.js
+++ js/leaflet_styleeditor/leaflet_styleeditor.drupal.js
@@ -6,10 +6,10 @@
   console.log("init StyleEditor_");
   $(document).on("leafletMapInit", function (e, settings, lMap, mapid) {
     console.log ("event  style editor");
-    if ((drupalSettings.leafletedit.styleeditor) && (drupalSettings.leafletedit.styleeditor.control)) {
+    if ((drupalSettings.leaflet_edit.styleeditor) && (drupalSettings.leaflet_edit.styleeditor.control)) {
       lMap.addControl(
         L.control.styleEditor({
-          position: drupalSettings.leafletedit.styleeditor.position,
+          position: drupalSettings.leaflet_edit.styleeditor.position,
         })
       );
     }
